3c63844e45bdb562fa8493d1a0745c37914cc312
Pade88 <ovidiu.padeanu@gmail.com>
2022-12-09 08:29:28 +0200

    day9 update

--- day9.py
+++ day9.py
@@ -1,55 +1,56 @@
-def partea_1(inputs):
-    # Desenez harta
-    head = (0, 0)
-    tail = [(0, 0) for _ in range(15)]
-    for line in my_inputs:
-        directia, cat = line.split()
-
-def partea_2(inputs):
-    import numpy
-    directii = {
+import numpy
+directii = {
     "U": numpy.array([-1, 0]),
     "D": numpy.array([1, 0]),
     "L": numpy.array([0, -1]),
     "R": numpy.array([0, 1]),
-    }
+}
 
-    def parse(in_line):
-        # convertesc directia de deplasare si valoarea
-        directia, cat = in_line.split()
-        cat = int(cat)
-        return directii[directia], cat
+def miscare(cap, coada):
+    di, dj = cap - coada
+    # Daca diferenta de coordonate e 0, atunci nu am miscare
+    if di == dj == 0:
+        return False
+    # Daca exista diferenta de coordonate, pe orice directie (abs)
+    if abs(di) > 1 or abs(dj) > 1:
+        coada[0] += numpy.sign(di)
+        coada[1] += numpy.sign(dj)
+    return True
 
-    def miscare(cap, coada):
-        # Schimb coordonatele capului si coadei
-        # sign returneaza -1, sau 1 in functie de tipul miscarii
-        di, dj = cap - coada
-        if di == 0 and abs(dj) >= 2:
-            coada[1] += numpy.sign(dj)
-        elif dj == 0 and abs(di) >= 2:
-            coada[0] += numpy.sign(di)
-        elif not (abs(di) <= 1 and abs(dj) <= 1):
-            coada[0] += numpy.sign(di)
-            coada[1] += numpy.sign(dj)
-
-    dimensiune_harta = 10
-    vizitat = set() # fara duplicate in punctele vizitate
-    # Desenez harta cu 0 de puncte (x, y)
-    harta = [numpy.array([0, 0]) for _ in range(dimensiune_harta)]
-
-    for line in inputs:
+def partea_1(my_inputs):
+    noduri = [numpy.array([0, 0]) for _ in range(10)]
+    v1 = {(0, 0)} # set in care pun pozitia de start (0, 0)
+    for line in my_inputs:
         # recuperez directia si valoarea
-        directia, cat = parse(line)
-        for _ in range(cat):
-            # adaug ultimul punct in setul de puncte vizitate
-            vizitat.add(tuple(harta[-1]))
-            # setez directia
-            harta[0] += directia
-            # deplasez capul si coada
-            for i in range(dimensiune_harta - 1):
-                miscare(harta[i], harta[i + 1])
+        directia, cat = line.split()
+        offset = directii[directia]
+        # Ma deplasez
+        for _ in range(int(cat)):
+            noduri[0] += offset
+            for i in range(9):
+                # Daca nu am diferenta intre noduri, trec mai departe
+                if not miscare(noduri[i], noduri[i + 1]):
+                    break
+            # Altfel adaug in set
+            v1.add(tuple(noduri[1]))
     # returnez numarul punctelor unice vizitate
-    return len(vizitat)
+    return len(v1)
+
+
+def partea_2(inputs):
+    noduri = [numpy.array([0, 0]) for _ in range(10)]
+    v1 = {(0, 0)}
+    for line in my_inputs:
+        directia, cat = line.split()
+        offset = directii[directia]
+        for _ in range(int(cat)):
+            noduri[0] += offset
+            for i in range(9):
+                if not miscare(noduri[i], noduri[i + 1]):
+                    break
+            v1.add(tuple(noduri[-1]))
+    # returnez numarul punctelor unice vizitate
+    return len(v1)
 
 if __name__ == "__main__":
     my_inputs = open("input_day_9.txt", "r").read().split('\n')
