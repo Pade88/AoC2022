3753b4f201a17d4c8632fb6246fffb8b2d33903d
Pade88 <ovidiu.padeanu@gmail.com>
2022-12-13 08:21:43 +0200

    day13 2/2

--- day13.py
+++ day13.py
@@ -19,7 +19,7 @@ def compare(nr1, nr2):
 
 
 def partea_1(my_inputs):
-    my_inputs = list(map(str.splitlines, my_inputs))
+    my_inputs = list(map(str.splitlines, my_inputs.split('\n\n')))
     counter = 0
     for idx, (a, b) in enumerate(my_inputs):
         a, b = eval(a), eval(b) # lista la string
@@ -28,9 +28,24 @@ def partea_1(my_inputs):
     return counter
 
 def partea_2(my_inputs):
-    pass
+    # lista la string
+    my_inputs = list(map(eval, my_inputs.split()))
+    # 0 - idx2 1 - idx6
+    idx = [1, 2]
+    for val in my_inputs:
+        # > 2 creste si idx2 si idx6
+        if compare(val, [[2]]) < 0:
+            idx[0] += 1
+            idx[1] += 1
+        # Doar idx6
+        elif compare(val, [[6]]) < 0:
+            idx[1] += 1
+
+    import math
+    # idx2 * idx2
+    return math.prod(idx)
 
 if __name__ == "__main__":
-    my_inputs = open("input_day_13.txt", "r").read().strip().split('\n\n')
+    my_inputs = open("input_day_13.txt", "r").read().strip()
     print(f'Solutia partea 1: {partea_1(my_inputs)}')
     print(f'Solutia partea 2: {partea_2(my_inputs)}')
